44f8816df13aeaf5ec3cd911090fce8971c9347c
masa48 <u.masaya48@gmail.com>
2015-11-08 05:39:20 +0000

    revision

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,7 +8,7 @@ class MicropostsController < ApplicationController
       redirect_to root_url
     else
       @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
-      render 'static_page/home'
+      render 'static_pages/home'
     end
   end
   
